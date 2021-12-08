ddb34062e8013555fc438e7f52c3e2492b729949
anna.tiala <anna.tiala@siili.com>
2021-12-08 11:24:41 +0200

    adding new components, update packages

---
+++ src/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import MessageList from "./components/message-list";
+
+import "./index.css";
+
+const NewApp = require("./components/message-list").default;
+
+function renderApp(App) {
+  ReactDOM.render(<App />, document.getElementById("root"));
+}
+
+renderApp(MessageList);
+
+if (module.hot) {
+  module.hot.accept("./components/message-list", () => {
+    renderApp(NewApp);
+  });
+}
